ed832e7db72e111f1f3894fd5ce1492cbc35d8da
liumi <liuming_365@hotmail.com>
2018-01-13 11:28:46 +0800

    add test

--- Test.py
+++ Test.py
@@ -1,2 +1,12 @@
-import numpy as np
-print(np.__version__)
+import tensorflow as tf
+
+a = 2
+b = 3
+x = tf.add(a, b)
+y = tf.multiply(a, b)
+useless = tf.multiply(a, x)
+z = tf.pow(y, x)
+with tf.Session() as sess:
+    writer = tf.summary.FileWriter("./graphs", sess.graph)
+    z = sess.run(z)
+    writer.close()
